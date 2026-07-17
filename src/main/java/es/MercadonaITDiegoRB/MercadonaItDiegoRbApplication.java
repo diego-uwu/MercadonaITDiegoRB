4353e2cfe2f9e41b17e50322685416681904776c
Arunikki <diego250499@gmail.com>
2026-07-17 15:43:16 +0200

    Initial commit

---
+++ src/main/java/es/MercadonaITDiegoRB/MercadonaItDiegoRbApplication.java
@@ -0,0 +1,13 @@
+package es.MercadonaITDiegoRB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadonaItDiegoRbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadonaItDiegoRbApplication.class, args);
+	}
+
+}
